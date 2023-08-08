377e382f5bb5a56a242205446176ad0de9a9e3e4
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-08 16:16:28 +0600

    Modified strings.py file

--- strings.py
+++ strings.py
@@ -44,3 +44,48 @@ last_name = "Hussain"
 full_name = first_name + " " + last_name # this concatination is valid but there formatting can also be used
 full_name = f"{first_name} {last_name}"  # f/F both can be used and get the same results as above concatination
 print(full_name)
+
+# String methods:
+''' In Python, there are few functions that are specific to string. Python everything is object.
+example, python_course is an object then put dot(.) after the object to get access to the set of python
+built-in functions more accuratly these are called Method '''
+
+python_course = "   Python language programming   "
+print(python_course.upper())
+
+''' This upper method return a new string of all capital letter by not affecting the original string.
+If I print(python_course) that will return orignal string. and we can store the new string in a new variable '''
+
+python_course_upper = python_course.upper()
+
+python_course_lower = python_course.lower()
+python_course_title = python_course.title()
+
+print(python_course_lower)
+print(python_course_title)
+
+python_course_remove_space = python_course.strip()
+print(python_course_remove_space)
+
+python_course_remove_space_on_left = python_course.lstrip()
+print(python_course_remove_space_on_left)
+
+python_course_remove_space_on_right = python_course.rstrip()
+print(python_course_remove_space_on_right)
+
+# Find method return index
+python_course_find_the_index_of_pro = python_course.find("pro")
+print(python_course_find_the_index_of_pro)
+
+#replace method
+python_course_replace_p_with_k = python_course.replace("p", "k")
+print(python_course_replace_p_with_k)
+
+''' Use in oparator to find the existence of a character or a sequence of characters in string.
+This will return boolean True/false '''
+print("pro" in python_course)
+
+''' This is an expression. Expression is a piece of codes that expresses the value '''
+
+# Use not in oparator
+print("pro" not in python_course)
