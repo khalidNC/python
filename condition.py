eef9893259dafd821189a2708b9a6a854cc0bbcf
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-08 21:01:04 +0600

    Modifed condition.py file

--- condition.py
+++ condition.py
@@ -12,11 +12,49 @@ Note: when we use if statement the expression will be ended with a punctuation c
 if temparature > 30:
   print("It's hot")         # there is 2space indentation, that implies these statements will execute under if.
   print("Drink water")      # As many as statements with the indentation will be executed
+elif temparature > 20:      # We can use multiple if conditions as many as we need by elif
+  print("It is nice")
 print("Done!")              # There is no indentation so this statement is out of the if condition
 
-age = 25
 
-if age > 45:
-  print('Go to retirement')
+''' let's have the temparature below 30, then following statements will not execute 
+since the boolean expression return False. '''
+temparature = 10
+
+if temparature > 30:
+  print("It's hot")         
+  print("Drink water")
+print("Done!")
+
+
+''' Let's have the else condition now to excute other conditions '''
+if temparature > 30:
+  print("It's hot")         
+  print("Drink water")
+else:                             # If none of the if condition is Ture the use else condition
+  print("It's cold")
+print("Done!")
+
+
+''' Technique to write cleaner code. and the Usage of Ternary Operator. '''
+''' Let's assume we are writing a program for a office where the applicant's age must be less than or equal 30
+else they will not eligible. '''
+
+age = 28
+
+if age <= 30:
+  print("Eligible")
 else:
-  print('You still have time to work')
+  print("Not eligible")
+
+# There is no problem with the above codes. But a cleaner way to achive the same result
+if age <= 30:
+  message = "Eligible"
+else:
+  message = "Not eligible"
+
+print(message)
+
+# Even more cleaner way. 
+message = "Eligible" if age <= 30 else "Not eligible"
+print(message)
