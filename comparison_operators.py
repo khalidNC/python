409e9a7eebfdea4b486e3ff649ff9e3f9928bf75
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-08 18:46:29 +0600

    Added and modified a new file comparison_operators.py

---
+++ comparison_operators.py
@@ -0,0 +1,22 @@
+''' We use comparison operator to confirm value. '''
+
+print(10 > 3)             # Returns True
+print(10 >= 3)            # Returns True
+print(10 > 30)            # Returns True
+print(10 >= 30)           # Returns True
+print(10 == 10)           # Returns True
+print(10 == "10")         # Returns False
+print(10 != "10")         # Returns True
+
+# Comparison operator use in stirng
+
+print("football" > "baseball")      # will return True because football is written before gether than sign
+print("football" == "Football")     
+
+''' will return False because every characters here has numeric presentation. 
+So numeric value of "f" and "F" are not same'''
+
+print(ord("f"))
+print(ord("F"))
+
+''' To check numeric presentation of "f" and "F" we call the method ord() and compaire these are not same '''
