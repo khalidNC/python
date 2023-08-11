694c839e87e77f0919f4fc19d4e6121edc0b6ef8
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-11 18:38:15 +0600

    Added and modified the defining_functions.py file

---
+++ defining_functions.py
@@ -0,0 +1,25 @@
+''' So far we use the Python built in functions. Now we will see how create
+a custom function.
+First we need to difing the function as "def"
+Then we need to give a name of the function like variable.
+Function name should be meaningful, lowercase and words separated by underscore.
+Then a parenthesis and end with colons.
+Then it makes a auto endentation that means the following statements are belong to the function.
+Now the function is and we need to call it as we call the built-in function. 
+We need to remove the indentation and add 2 lines break after the function.
+Which is pep8(Python style guide) requirments. '''
+
+def greetings():
+    print("Welcome Khalid")
+    print("Welcome to the python world")
+
+
+greetings()
+
+# Another example;
+def car_wash():
+    print("The car is dirty")
+    print("The car needs servicing")
+
+
+car_wash()
