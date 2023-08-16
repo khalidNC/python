1a044f8ee11fe300c58aff76cbb0e4318042df56
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-16 16:21:12 +0600

    Modified data_structure_list_sorting.py file

---
+++ Data structure/data_structure_list_sorting.py
@@ -0,0 +1,6 @@
+''' Ascending order: rearranging list items with ascending order se use sort() method.
+Example we have list of numbers that have no perticular order. '''
+
+numbers = [1, 2, 5, 9, 0, 23, 54, 17, 12]
+numbers.sort()
+print(numbers)
