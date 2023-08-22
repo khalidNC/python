766267a760c6755e074ecf558734480204dd979b
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-22 15:57:46 +0600

    Added and modified exception.py

---
+++ Exceptions/exception.py
@@ -0,0 +1,16 @@
+''' n Python, an exception is an event that occurs during the execution of a program that disrupts the normal flow of code. Exceptions are used to handle errors, unexpected situations, and exceptional conditions that might arise during the execution of a program. Python provides a robust mechanism for handling exceptions through the use of try, except, and related statements.
+
+Here's how exceptions work in Python:
+
+Exception Types:
+Python has a variety of built-in exception types, such as ZeroDivisionError, TypeError, ValueError, FileNotFoundError, and many more. Each exception type represents a specific kind of error that can occur.
+
+Try-Except Block:
+The try block contains the code where you expect an exception to occur. If an exception occurs within the try block, the program jumps to the corresponding except block that handles that specific type of exception.
+
+Handling Exceptions:
+In the except block, you specify the type of exception you want to catch and handle. You can also include additional logic to manage the exception, such as printing an error message or taking corrective action.
+
+Here's a basic example demonstrating how to use exceptions in Python:
+
+'''
