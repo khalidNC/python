b03e4a35cce8778447da02355ec03ab75db3bc7b
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-26 23:53:22 +0600

    modified classes.py

--- Classes/classes.py
+++ Classes/classes.py
@@ -12,3 +12,23 @@ shopping_cart = []
 shopping_cart.add()
 shopping_cart.remove()
 shopping_cart.get_total()
+
+''' Defining a Class:
+You define a class using the class keyword. Here's a basic example of a Person class: '''
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+''' In this example, we've defined a class called Person with two attributes (name and age) and a method (greet).
+The __init__ method is a special method called a constructor. It initializes the object's attributes when an object 
+is created. The self parameter refers to the object being created. The greet method is a simple method that prints 
+out a greeting using the object's attributes. '''
+
+''' Creating Objects:
+Once you have a class, you can create objects (instances) of that class. Here's how you do it: '''
+
