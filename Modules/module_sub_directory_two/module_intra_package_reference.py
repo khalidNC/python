fb64cf681d0152640b3ace0edc13d8375cc70e79
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-09-28 16:29:52 +0600

    updated module_intra_package_reference.py

--- Modules/module_sub_directory_two/module_intra_package_reference.py
+++ Modules/module_sub_directory_two/module_intra_package_reference.py
@@ -1,4 +1,8 @@
+# Absolute import
 from Modules.module_sub_directory_one import module_intra_package
+
+# Relative import
+from ..module_sub_directory_one import module_intra_package
 
 
 ''' There are times when need to import form the sibling packages. To do so we created a new 
@@ -6,10 +10,11 @@ packages(module_sub_directory_one, module_sub_directory_two). Then created a py 
 (module_intra_package.py, module_intra_package_reference.py)
 
 Now we will import this module or bojects of this module on module_intra_package_reference.py file which is under 
-a sibling package: module_sub_directory_two. So let's add some functions here and go back to the 
-module_intra_package_reference.py file and import this module. 
+a sibling package: module_sub_directory_two. So let's add some functions there and import the module here. 
 
-Before import add __init__.py file here in the folder to make it a package. '''
+Before import add __init__.py file here in the folder to make it a package. 
+
+We can import in 2 ways: absolute import and relative import'''
 
 
 def customer_info(first_name, last_name, contact_number):
@@ -17,4 +22,5 @@ def customer_info(first_name, last_name, contact_number):
 
 company_name = "ABC Corp"
 
-print(module_intra_package.product_sku(23456))
+module_intra_package.product_sku(23456)
+module_intra_package.sku
