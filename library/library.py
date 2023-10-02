754ed64a7d096950905fc08b05ad15a58dc6ed0b
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-10-02 18:10:11 +0600

    Created and updated library.py

---
+++ library/library.py
@@ -0,0 +1,12 @@
+''' Python Standard Lbrary:
+
+Python has a 'batary included' philosophy, which means it comes with comprehensive library packages
+and moduels that provide comman features we need when building real aplication.
+
+Here we will explore python standard library. More specificly we will learn on:
+Files - how to work with directories
+SQLite - how to deal with SQLite database
+Date/Time - Date and time object 
+Random values - generate random balues
+Emails - sending emails
+and so on. '''
