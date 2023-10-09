371fe8cabac63ef6d9ae1821370d13a8fbc05659
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-10-09 23:26:56 +0600

    Updated library_files.py

---
+++ library/library_files.py
@@ -0,0 +1,81 @@
+''' Python provides a rich set of libraries and modules for working with files.
+These libraries enable you to perform various operations on files, such as reading, 
+writing, appending, and more. Here's an overview of some of the key libraries and 
+functions for working with files in Python: '''
+
+''' Built-in open() Function:
+Python's built-in open() function is used to open files. It takes two arguments: 
+the file path and the mode (e.g., 'r' for reading, 'w' for writing, 'a' for appending, etc.).
+Example: '''
+
+with open('example.txt', 'r') as file:
+    content = file.read()
+
+
+''' Reading Files:
+You can read the contents of a file using methods like read(), readline(), or readlines() '''
+
+with open('example.txt', 'r') as file:
+    content = file.read()  # Read the entire file
+    line = file.readline()  # Read one line at a time
+    lines = file.readlines()  # Read all lines into a list
+
+
+''' Writing and Appending Files:
+To write to a file, open it in write ('w') or append ('a') mode and use write() method. '''
+
+with open('example.txt', 'w') as file:
+    file.write('Hello, World!')
+
+with open('example.txt', 'a') as file:
+    file.write('\nAppended line')
+
+
+''' Iterating Over Lines:
+You can iterate over the lines of a file using a for loop. '''
+
+with open('example.txt', 'r') as file:
+    for line in file:
+        print(line)
+
+
+''' File Handling in a Context:
+The with statement is commonly used to ensure proper file handling. It automatically closes 
+the file when you're done.
+
+Working with Binary Files:
+To work with binary files, you can use modes like 'rb' (read binary) or 'wb' (write binary) 
+when opening files. '''
+
+with open('image.jpg', 'rb') as binary_file:
+    image_data = binary_file.read()
+
+
+''' File Path Handling:
+The os module provides functions for working with file paths, such as os.path.join() to create 
+file paths and os.path.exists() to check if a file exists. '''
+
+import os
+
+path = os.path.join('folder', 'file.txt')
+exists = os.path.exists(path)
+
+
+''' Exception Handling:
+When working with files, it's important to handle exceptions, such as FileNotFoundError and PermissionError, 
+to gracefully deal with file-related issues. '''
+
+try:
+    with open('nonexistent.txt', 'r') as file:
+        content = file.read()
+
+except FileNotFoundError:
+    print("File not found.")
+
+except PermissionError:
+    print("Permission denied.")
+
+
+''' These are the fundamental tools and concepts for working with files in Python. Depending on your specific 
+use case, you may also want to explore libraries like os, shutil (for file operations), and libraries for 
+working with different file formats (e.g., csv, json, pickle, sqlite3, etc.). '''
