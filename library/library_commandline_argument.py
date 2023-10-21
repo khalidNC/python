4302d8b1be33a38aeb281a99c826463712dd48da
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-10-21 21:02:04 +0600

    Created and updated library_commandline_argument.py

---
+++ library/library_commandline_argument.py
@@ -0,0 +1,5 @@
+''' Command line arguments allow you to pass input parameters to your Python scripts when 
+they are executed from the command line. Python provides the argparse library as part of 
+the standard library to easily handle command line arguments. The argparse library simplifies 
+the process of defining and parsing command line arguments, making it a powerful tool for 
+creating flexible and user-friendly command line interfaces for your Python programs. '''
