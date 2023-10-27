39e400f5bdc9a46e2a9929578627422923af0d62
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-10-27 23:14:34 +0600

    updated virtual_environment.py

---
+++ packages/virtual_environment.py
@@ -0,0 +1,7 @@
+''' A virtual environment in Python is a self-contained directory that houses a Python interpreter 
+and its own set of libraries. It is used to isolate and manage Python packages and dependencies 
+for different projects, ensuring that one project's dependencies do not interfere with another. 
+Virtual environments are particularly useful when you have multiple Python projects with different 
+library requirements.
+
+Here's a explanation with an example of how to create and use virtual environments in Python: '''
