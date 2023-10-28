d1172bac8c6469b0eb162861fd4aab313a7cf11a
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-10-28 20:57:32 +0600

    updated virtual_environment.py

--- packages/virtual_environment.py
+++ packages/virtual_environment.py
@@ -5,3 +5,18 @@ Virtual environments are particularly useful when you have multiple Python proje
 library requirements.
 
 Here's a explanation with an example of how to create and use virtual environments in Python: '''
+
+''' 1. Creating a Virtual Environment:
+
+You can create a virtual environment using the venv module (for Python 3.3 and later) or virtualenv 
+(an external package).
+
+Using venv: 
+python -m venv myenv 
+
+Using virtualenv (if not installed, you can install it via pip):
+pip install virtualenv
+virtualenv myenv
+
+Replace myenv with the name you want to give to your virtual environment. '''
+
