f241d7cefaaa08f70524afaf87d13dea6c2d5ce8
KhalidNC <hussainkhalid70@yahoo.com>
2023-11-01 19:47:07 +0600

    deleted virtual env

---
+++ packages/pipenv.py
@@ -0,0 +1,10 @@
+''' So far we know to use pip and know how to create virtual environment. 
+And all the terminal command we need memorise there are lot's of things we momorise 
+to make our life easy pipenv comes into the picture. This pipenv is a tool that 
+combines both needs together. So we do not have to use pip and vertual environment
+separately. It is a dependescy manager in python pacakages. 
+
+So let's use pipenv. First we need to install pipenv. So write and run this command in 
+terminal; 
+1. pip3 install pipenv 
+2. After pipenv installed, we install packages like, request '''
