234781f71dfde99a9754054b7f60e98956eb6a1a
KhalidNC <hussainkhalid70@yahoo.com>
2023-11-10 22:08:55 +0600

    Updated pipfile.py

--- packages/pipfile.py
+++ packages/pipfile.py
@@ -1,2 +1,11 @@
 ''' When we install a packagess useing pipenv 2 files are automatically created, Pipfile and Pipfile.lock these
-2 files are use to keep track of the dependencies of our project '''
+2 files are use to keep track of the dependencies of our project 
+
+For example; we open the Pipfile then we see basicallay below thigns;
+The primary purpose of a Pipfile is to specify the dependencies for your Python project. It lists all the packages 
+and their versions required for your project to run correctly. These dependencies are typically stored under two 
+sections: [packages] and [dev-packages].
+
+[packages]: This section contains the main project dependencies required for the application to function in production.
+[dev-packages]: This section contains dependencies required for development, such as testing frameworks, linters, 
+or tools used during development. '''
